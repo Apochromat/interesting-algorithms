a2b58746b12ceadbf23916b41048c8d7359d90b1
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-03-16 23:26:41 +0700

    Ползунок теперь регулирует размер таблицы

--- algorithm_a/table.js
+++ algorithm_a/table.js
@@ -9,39 +9,32 @@ function ready() {
 }
 
 function tableCreate() {
-  //body reference
+  if (document.getElementById("ioTable") != null) {
+    var todel = document.getElementById("ioTable");
+    todel.remove();
+  }
+  var size = parseInt(document.getElementById("tableSizeSlider").value);
   var body = document.getElementsByClassName("algorithmView")[0];
-
-  // create elements <table> and a <tbody>
   var tbl = document.createElement("table");
   var tblBody = document.createElement("tbody");
-
-  // cells creation
-  for (var j = 0; j <= 2; j++) {
-    // table row creation
+  for (var j = 0; j <= size; j++) {
     var row = document.createElement("tr");
 
-    for (var i = 0; i < 2; i++) {
-      // create element <td> and text node
-      //Make text node the contents of <td> element
-      // put <td> at end of the table row
+    for (var i = 0; i < size; i++) {
       var cell = document.createElement("td");
-      var cellText = document.createTextNode(
-        "cell is row " + j + ", column " + i
-      );
-
+      var cellText = document.createTextNode(" ");
+      cell.setAttribute("border", "2px");
+      cell.setAttribute("width", "25px");
+      cell.setAttribute("height", "25px");
       cell.appendChild(cellText);
       row.appendChild(cell);
     }
 
-    //row added to end of table body
     tblBody.appendChild(row);
   }
 
-  // append the <tbody> inside the <table>
   tbl.appendChild(tblBody);
-  // put <table> in the <body>
   body.appendChild(tbl);
-  // tbl border attribute to
-  tbl.setAttribute("border", "2");
+  tbl.setAttribute("border", "2px");
+  tbl.setAttribute("id", "ioTable");
 }
