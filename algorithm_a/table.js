fb2c6ce94032123b7c7e17d988e22337c0a0d747
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-03-16 18:41:25 +0700

    Кнопочка строит тааблички

--- algorithm_a/table.js
+++ algorithm_a/table.js
@@ -1,7 +1,47 @@
+matrix = [];
 document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
-  document.querySelector("#buildTable").onclick = function() {
-    alert("Hi");
+  document.querySelector("#buildTable").onclick = function () {
+    tableCreate();
+    console.log("Hi");
+  };
+}
+
+function tableCreate() {
+  //body reference
+  var body = document.getElementsByClassName("algorithmView")[0];
+
+  // create elements <table> and a <tbody>
+  var tbl = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  // cells creation
+  for (var j = 0; j <= 2; j++) {
+    // table row creation
+    var row = document.createElement("tr");
+
+    for (var i = 0; i < 2; i++) {
+      // create element <td> and text node
+      //Make text node the contents of <td> element
+      // put <td> at end of the table row
+      var cell = document.createElement("td");
+      var cellText = document.createTextNode(
+        "cell is row " + j + ", column " + i
+      );
+
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    //row added to end of table body
+    tblBody.appendChild(row);
   }
+
+  // append the <tbody> inside the <table>
+  tbl.appendChild(tblBody);
+  // put <table> in the <body>
+  body.appendChild(tbl);
+  // tbl border attribute to
+  tbl.setAttribute("border", "2");
 }
