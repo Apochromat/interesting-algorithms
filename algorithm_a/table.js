bc3125bd0299f091115449904caaffe53932c23b
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-03-17 19:11:46 +0700

    Реализовал безошибочное рисование

--- algorithm_a/table.js
+++ algorithm_a/table.js
@@ -1,6 +1,17 @@
-var state = -1; // Режим рисования: 0 - очистить, 1 - вход, 2 - выход, -1 - стенка
-document.addEventListener("DOMContentLoaded", ready);
+var objectTable = {
+  size: 0,              //Размер таблицы, сторона квадрата
+  table: null,          //Объект-ссылка на таблицу
+  cellInput: null,      //Объект-ссылка на начальную клетку
+  cellOutput: null,     //Объект-ссылка на конечную клетку
+  cellList: [],         //Массив объектов-ссылок на все ячейки в виде списка для перебора
+  cellMatrix: [],       //Массив объектов-ссылок на все ячейки в виде двумерного массива
+  matrix: []            //Массив типов (clear, input, output, wall) ячеек в виде двумерного массива
+};
 
+var state = -1; // Режим рисования: 0 - очистить, 1 - вход, 2 - выход, -1 - стенка, 3 - просмотрено, 4 - правильный путь
+
+//Выполнение при инициализации, привязка Слушателей
+document.addEventListener("DOMContentLoaded", ready);
 function ready() {
   document.querySelector("#buildTable").onclick = function () {
     tableCreate();
@@ -23,56 +34,81 @@ function buildMaze() {
   console.log("Build Maze");
 }
 
+function run() {
+  console.log("Run");
+}
+
+//Функции для рисования
 function setPaintState(newState) {
   state = newState;
   console.log(`State: ${state}`);
 }
 
 function clearAll() {
-  for (element of document.getElementsByClassName("ioTableCell")) {
-    element.setAttribute("bgcolor", `white`);
+  for (element of objectTable.cellList) {
+    element.setAttribute("class", `ioTableCell clear`);
   }
 }
 
-function run() {
-  console.log("Run");
-}
-
-function coloriseCell(cell) {
-  console.log("Colorise: " + state)
-  switch (state) {
-    case 0:
-      cell.setAttribute("bgcolor", `white`);
+//Раскраска ячейки по глобальному state, если он не передается в функцию, strictState требуется для покраски в реальном времени
+function coloriseCell(cell, strictState=0) {
+  switch (strictState == 0 ? state : strictState) {
+    case 0:   //Clear
+      cell.setAttribute("class", `ioTableCell clear`);
+      if (cell == objectTable.cellInput) {
+        objectTable.cellInput = null;
+      }
+      if (cell == objectTable.cellOutput) {
+        objectTable.cellOutput = null;
+      }
       break;
-    case -1:
-      cell.setAttribute("bgcolor", `#616161`);
+    case -1:  //Wall
+      cell.setAttribute("class", `ioTableCell wall`);
+      if (cell == objectTable.cellInput) {
+        objectTable.cellInput = null;
+      }
+      if (cell == objectTable.cellOutput) {
+        objectTable.cellOutput = null;
+      }
       break;
-    case 1:
-      cell.setAttribute("bgcolor", `#0a76d0`);
+    case 1:   //Input
+      cell.setAttribute("class", `ioTableCell input`);
+      if (objectTable.cellInput != null) {
+        objectTable.cellInput.setAttribute("class", `ioTableCell clear`);
+      }
+      objectTable.cellInput = cell;
       break;
-    case 2:
-      cell.setAttribute("bgcolor", `#ffa500`);
+    case 2:   //Output
+      cell.setAttribute("class", `ioTableCell output`);
+      if (objectTable.cellOutput != null) {
+        objectTable.cellOutput.setAttribute("class", `ioTableCell clear`);
+      }
+      objectTable.cellOutput = cell;
+      break;
+    case 3:   //Visited
+      cell.setAttribute("class", `ioTableCell visited`);
+      break;
+    case 4:   //Way
+      cell.setAttribute("class", `ioTableCell way`);
       break;
   }
 }
 
+//"Подгон" размера ячеек таблицы, чтобы они были квадратными и занимали всю доступную площадь
 function tableSizeAdjust() {
-  var cells = document.getElementsByClassName("ioTableCell");
-  var table = document.getElementById("ioTable");
-  var boxSize = Math.min(window.screen.height - 300, window.screen.width - document.querySelector('.algorithmOptions').offsetWidth)
-  var cellAmount = parseInt(document.getElementById("tableSizeSlider").value);
-  table.setAttribute("width", `${boxSize}px`);
-  table.setAttribute("height", `${boxSize}px`);
-  for (element of cells) {
-    element.setAttribute("width", `${Math.floor(boxSize / cellAmount)}px`);
-    element.setAttribute("height", `${Math.floor(boxSize / cellAmount)-3}px`);
-    element.setAttribute("bgcolor", `white`);
+  var boxSize = Math.min(window.innerHeight - 180, window.innerWidth - document.querySelector('.algorithmOptions').offsetWidth - 200);
+  objectTable.table.setAttribute("width", `${boxSize}px`);
+  objectTable.table.setAttribute("height", `${boxSize}px`);
+  for (element of objectTable.cellList) {
+    element.setAttribute("width", `${Math.floor(boxSize / objectTable.size)}px`);
+    element.setAttribute("height", `${Math.floor(boxSize / objectTable.size) - 3}px`);
     element.onclick = function(e){
       coloriseCell(e.target);
     }
   }
 }
 
+//Создание таблицы, установление objectTable для взаимодействия
 function tableCreate() {
   if (document.getElementById("ioTable") != null) {
     var todel = document.getElementById("ioTable");
@@ -86,12 +122,8 @@ function tableCreate() {
     var row = document.createElement("tr");
     for (var i = 0; i < size; i++) {
       var cell = document.createElement("td");
-      var cellText = document.createTextNode(" ");
       cell.setAttribute("border", "1px");
-      cell.setAttribute("class", "ioTableCell");
-      cell.setAttribute("width", "25px");
-      cell.setAttribute("height", "25px");
-      cell.appendChild(cellText);
+      cell.setAttribute("class", "ioTableCell clear");
       row.appendChild(cell);
     }
     tableBody.appendChild(row);
@@ -100,4 +132,16 @@ function tableCreate() {
   algorithmView.appendChild(table);
   table.setAttribute("border", "1px");
   table.setAttribute("id", "ioTable");
+  objectTable.size = size;
+  let matrix = [];
+  for (element of document.getElementsByClassName("ioTableCell")) {
+    objectTable.cellList.push(element);
+    if (matrix.length == size) {
+      objectTable.cellMatrix.push(matrix);
+      matrix = [];
+    }
+    matrix.push(element);
+  }
+  objectTable.cellMatrix.push(matrix);
+  objectTable.table = document.getElementById("ioTable");
 }
