040c50a761435714c67330996f9b5b200eff8b1c
PodsolnyX <podsolnyx03@mail.ru>
2022-03-18 20:55:01 +0700

    test

---
+++ algorithm_clustering/cluster.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("canvasCluster");
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth*0.6;
+canvas.height = window.innerHeight*0.6;
+ctx.fillStyle = "gray";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
