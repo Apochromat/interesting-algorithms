7c091f6c238750458e60c9dd704556c77d408ece
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2022-03-29 23:19:31 +0700

    Строится объект дерева, классифицируется

--- decision_tree/script.js
+++ decision_tree/script.js
@@ -1,5 +1,8 @@
 const reader = new FileReader()
 var tree;
+var nonNumberPropertyValuesAmount = 5;
+var minEntropy = 0.05;
+var minDeltaEntropy = 0.01;
 //Выполнение при инициализации, привязка обработчиков
 document.addEventListener("DOMContentLoaded", ready);
 function ready() {
@@ -31,23 +34,76 @@ function csvToArray(str, delimiter = ";") {
 
 function run() {
   console.log(tree.calculateEntropy(tree.csvData));
+  tree.head.split(tree);
+  console.log(tree);
+}
+
+
+class Node {
+  constructor(batch, index, depth, type = "node"){
+    this.type = type;
+    this.depth = depth;
+    this.label;
+    this.index = index;
+    this.batch = batch;
+    this.childs = [];
+    this.property;
+    this.condition;
+    this.value;
+  }
+  split(tree){
+    if (tree.findBestSplit(this.batch) == undefined) {
+      this.type = "leaf";
+    }
+    else {
+      var predict = tree.findBestSplit(this.batch);
+      if (predict["condition"] == "equal"){
+        this.label = `Divide by ${predict["property"]}`;
+        this.property = predict["property"];
+      }
+      else {
+        this.label = `Is ${predict["property"]} ${predict["condition"]} than ${predict["value"]}?`;
+        this.property = predict["property"];
+        this.condition = predict["condition"];
+        this.value = predict["value"];
+      }
+      if ((predict["entropy"][0] > minEntropy) || (predict["entropy"][1] > minDeltaEntropy)) {
+        for (let i = 0; i < predict["split"].length; i++) {
+          this.childs.push(new Node(predict["split"][i], i, this.depth+1));
+        }
+        for (let el of this.childs) {
+          el.split(tree);
+        }
+      }
+      else {
+        this.type = "leaf";
+      }
+    }
+  }
+  feedForwand(input){
+    console.log(input);
+  }
 }
 
 class Tree {
   constructor(csvData) {
     this.csvData = csvData;
     this.keys = Object.keys(csvData[0]);
+    this.keys.splice(this.keys.indexOf("Id"), 1);
     this.propertyValues = {};
+    this.head = new Node(csvData, 0, 0);
   }
+
   calculatePropertyValues(){
     for (let key of this.keys) {
       let temp = new Set();
       for (let index = 0; index < this.csvData.length; index++) {
         temp.add(this.csvData[index][key]);
       }
-      this.propertyValues[key] = temp;
+      this.propertyValues[key] = new Set(Array.from(temp).sort((a, b) => a-b));
     }
   }
+
   calculateEntries(batch, propertyValue, property){
     let entries = 0;
     for (let el of batch) {
@@ -57,11 +113,12 @@ class Tree {
     }
     return entries
   }
+
   calculateEntropy(batch, property = "Class") {
     let h = 0;
     for (let propertyValue of this.propertyValues[property]){
       let p = this.calculateEntries(batch, propertyValue, property)/batch.length;
-      h -= p*Math.log2(p);
+      if (p != 0) h -= p*Math.log2(p);
     }
     return h
   }
@@ -79,22 +136,22 @@ class Tree {
           }
           break;
         case "more":
-          if (el[property] > propertyValue){
+          if (parseFloat(el[property]) > parseFloat(propertyValue)){
             filtred.push(el);
           }
           break;
         case "less":
-          if (el[property] < propertyValue){
+          if (parseFloat(el[property]) < parseFloat(propertyValue)){
             filtred.push(el);
           }
           break;
         case "nomore":
-          if (el[property] <= propertyValue){
+          if (parseFloat(el[property]) <= parseFloat(propertyValue)){
             filtred.push(el);
           }
           break;
         case "noless":
-          if (el[property] >= propertyValue){
+          if (parseFloat(el[property]) >= parseFloat(propertyValue)){
             filtred.push(el);
           }
           break;
@@ -104,4 +161,53 @@ class Tree {
     return filtred
   }
 
+  findBestSplit(batch) {
+    var initialEntropy = this.calculateEntropy(batch);
+    let bestDeltaEntropy = 0;
+    let predict;
+    for (let key of this.keys) {
+      if (key == "Class") continue;
+      var tempSplit = [];
+      if (this.propertyValues[key].size > nonNumberPropertyValuesAmount){
+        for (let propertyValue of this.propertyValues[key]) {
+          tempSplit = [this.filterByProperty(batch, propertyValue, key, "more"), this.filterByProperty(batch, propertyValue, key, "nomore")];
+          if ((tempSplit[0].length != 0)&&(tempSplit[1].length != 0)) {
+            let tempEntropy = Math.max(this.calculateEntropy(tempSplit[0]) + this.calculateEntropy(tempSplit[1]));
+            if ((initialEntropy - tempEntropy) > bestDeltaEntropy) {
+              bestDeltaEntropy = initialEntropy - tempEntropy;
+              predict = {
+                "split": tempSplit,
+                "condition": "more",
+                "property": key,
+                "value": propertyValue, 
+                "entropy": [tempEntropy, bestDeltaEntropy]
+              }
+            }
+          }
+        } 
+      }
+      else {
+        let tempEntropySum = 0;
+        let tempEntropyObjects = 0;
+        for (let propertyValue of this.propertyValues[key]) {
+          let filtered = this.filterByProperty(batch, propertyValue, key, "equal");
+          tempSplit.push(filtered);
+          tempEntropySum += this.calculateEntropy(filtered);
+          tempEntropyObjects++;          
+        }
+        let tempEntropy = tempEntropySum / tempEntropyObjects;
+        if ((initialEntropy - tempEntropy) > bestDeltaEntropy) {
+          bestDeltaEntropy = initialEntropy - tempEntropy;
+          predict = {
+            "split": tempSplit,
+            "condition": "equal",
+            "property": key,
+            "value": null,
+            "entropy": [tempEntropy, bestDeltaEntropy]
+          }
+        }
+      }
+    } 
+    return predict
+  }
 }
